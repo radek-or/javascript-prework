249b4903e8605aff11458c8076bdf0962af09a64
Radosław Orczykowski <radek.or@gmail.com>
2024-03-19 22:00:54 +0100

    Add playerMove

--- js/functions.js
+++ js/functions.js
@@ -10,12 +10,26 @@ function clearMessages() {
 
 var computerMove, randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
-console.log('wylosowana liczba to: ' + randomNumber);
-if (randomNumber == '1') {
-  computerMove = 'kamień';
-} else if (randomNumber == '2') {
-  computerMove = 'papier';
-} else if (randomNumber == '3') {
-  computerMove = 'nożyce';
+console.log("wylosowana liczba to: " + randomNumber);
+if (randomNumber == "1") {
+	computerMove = "kamień";
+} else if (randomNumber == "2") {
+	computerMove = "papier";
+} else if (randomNumber == "3") {
+	computerMove = "nożyce";
 }
-printMessage('Mój ruch: ' + computerMove);
+printMessage("Mój ruch: " + computerMove);
+
+var playerMove, playerInput;
+playerInput = prompt("Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.");
+console.log("Wpisana odpowiedź to: " + playerInput);
+if (playerInput == "1") {
+	playerMove = "kamień";
+} else if (playerInput == "2") {
+	playerMove = "papier";
+} else if (playerInput == "3") {
+	playerMove = "nożyce";
+} else {
+	playerMove = "kamień";
+}
+printMessage("Twój ruch: " + playerMove);
