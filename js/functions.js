97ff80093283e3cbb20f1d8af7a49c2e40780f82
Radosław Orczykowski <radek.or@gmail.com>
2024-03-19 21:18:55 +0100

    Add new variable playerMove

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,19 @@
-function printMessage(msg){
-	var div = document.createElement('div');
+function printMessage(msg) {
+	var div = document.createElement("div");
 	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
+	document.getElementById("messages").appendChild(div);
 }
 
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
+function clearMessages() {
+	document.getElementById("messages").innerHTML = "";
 }
+
+var computerMove, playerMove;
+computerMove = "kamień";
+printMessage(
+	"Zagrałem " + computerMove + "! Jeśli Twój ruch to papier, to wygrywasz!"
+);
+playerMove = "papier";
+printMessage(
+	"Zagrałem " + playerMove + "! Jeśli Twój ruch to nożyce, to wygrywasz!"
+);
