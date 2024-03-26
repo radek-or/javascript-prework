c8038df92ff95a06449bca8afdc8df5fdbc8c8a3
Radosław Orczykowski <radek.or@gmail.com>
2024-03-26 18:19:23 +0100

    Change variable declarations

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ function buttonClicked(argButtonName) {
 	clearMessages();
 	console.log(argButtonName + " został kliknięty");
 
-	var computerMove, playerMove, randomNumber;
+	var playerMove, randomNumber;
 
 	function getMoveName(argMoveId) {
 		console.log("wywołano funkcję getMoveName z argumentem: " + argMoveId);
@@ -70,7 +70,7 @@ function buttonClicked(argButtonName) {
 	console.log("ruch gracza to: " + playerMove);
 	randomNumber = Math.floor(Math.random() * 3 + 1);
 	console.log("wylosowana liczba to: " + randomNumber);
-	computerMove = getMoveName(randomNumber);
+	const computerMove = getMoveName(randomNumber);
 	console.log("ruch komputera to: " + computerMove);
 	displayResult(playerMove, computerMove);
 }
